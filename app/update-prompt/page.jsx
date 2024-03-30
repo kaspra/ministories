6f7261ea4fc80a6b2d96539eb615dd6c051bb5ea
kaspra <92664226+kaspra@users.noreply.github.com>
2024-03-30 11:52:41 +0530

    commit

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -62,13 +62,15 @@ const EditPrompt = () => {
   };
 
   return (
-    <Form
-      type="Edit"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Form
+        type="Edit"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+      />
+    </Suspense>
   );
 };
 
