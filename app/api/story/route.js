498da41dcf7e813d2de30f0ff3f8015c1b623935
kaspra <92664226+kaspra@users.noreply.github.com>
2024-03-31 17:06:34 +0530

    version 1.0 commit

--- app/api/story/route.js
+++ app/api/story/route.js
@@ -1,12 +1,22 @@
 import Story from "@models/story";
 import { connectToDB } from "@utils/database";
+import { headers } from "next/headers";
 
 export const GET = async (request) => {
+    const headersList = headers();
+    const referer = headersList.get('referer');
+
     try {
         await connectToDB();
         const story = await Story.find({}).populate('creator')
 
-        return new Response(JSON.stringify(story), {status: 200})
+        return new Response(JSON.stringify(story), {
+            status: 200,
+            headers: {
+                referer: referer,
+                'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
+            },
+        })
     } catch (error) {
         return new Response("Failed to fetch all story",{status: 500})
     }
